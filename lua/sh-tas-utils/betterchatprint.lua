df75c73246e3c9270e21ecdb6d56a986e46a39a1
Derpius <49565664+Derpius@users.noreply.github.com>
2021-09-23 17:32:07 +0100

    Fix and improve chatprint

--- lua/sh-tas-utils/betterchatprint.lua
+++ lua/sh-tas-utils/betterchatprint.lua
@@ -8,7 +8,7 @@ if SERVER then
 		for _, v in ipairs({...}) do
 			if IsColor(v) then
 				header = header .. "c"
-				body = body .. string.format("%c%c%c\0", v:Unpack())
+				body = body .. string.format("%c%c%c", v:Unpack())
 			else
 				header = header .. "s"
 				body = body .. tostring(v) .. "\0"
@@ -26,42 +26,36 @@ else
 			error("Failed to decompress TASUtils.ChatPrint data")
 			return
 		end
-		len = #data
 
-		local i = 0
+		local dataPtr = 1
 
 		-- Parse header
 		local header, headerLen = "", 0
 		while true do
-			i = i + 1
 			if data[i] == "\0" then break end
+
 			header = header .. data[i]
 			headerLen = headerLen + 1
+			dataPtr = dataPtr + 1
 		end
 
+		dataPtr = dataPtr + 1 -- Skip over the null char that caused the above loop to break
+
 		-- Parse body
-		local args, headerLoc = {}, 1
-		buffer = ""
-		while true do
-			i = i + 1
-			if data[i] == "\0" then -- Handle buffer
-				if header[headerLoc] == "c" then
-					print(string.byte(buffer))
-					table.insert(args, Color(string.byte(buffer)))
-				elseif header[headerLoc] == "s" then
-					table.insert(args, buffer)
+		local args = {}
+		for i = 1, headerLen do
+			if header[i] == "c" then -- Colours
+				table.insert(args, Color(string.byte(data, dataPtr, dataPtr + 2)))
+				dataPtr = dataPtr + 3 -- Skip over the three colour values
+			elseif header[i] == "s" then -- Strings
+				local substring = ""
+				while data[dataPtr] ~= "\0" do
+					substring = substring .. data[dataPtr]
+					dataPtr = dataPtr + 1
 				end
+				dataPtr = dataPtr + 1 -- Skip over null char
 
-				if i == len then break end -- We've reached the final null char
-
-				headerLoc = headerLoc + 1
-				buffer = ""
-
-				if headerLoc > headerLen then
-					error("Body length longer than header")
-				end
-			else
-				buffer = buffer .. data[i]
+				table.insert(args, substring)
 			end
 		end
 
