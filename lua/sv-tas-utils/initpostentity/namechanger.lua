57ee98b3e6f5128fb4762b1a872d5abee7c53643
Derpius <49565664+Derpius@users.noreply.github.com>
2021-09-23 15:30:27 +0100

    Dev wiki hostname limit was exclusive

--- lua/sv-tas-utils/initpostentity/namechanger.lua
+++ lua/sv-tas-utils/initpostentity/namechanger.lua
@@ -2,21 +2,20 @@ local defaultName = "Total Anarchy Server [Wire|SfEx|CSLua] - "
 local rootName = CreateConVar("root_hostname", defaultName, FCVAR_ARCHIVE, "Server name to use before appending msg")
 local delay = CreateConVar("hostname_change_delay", 300, FCVAR_ARCHIVE, "Time to wait before changing hostname", 1)
 
---	"-----------------------"  How long one of these entries can be assuming we're using the default name above
+--	"----------------------"  How long one of these entries can be assuming we're using the default name above
 local names = {
-	"Site under construction",
 	"Better than s&box",
 	"Complaining about Lua",
 	"Surprise restarts",
 	"Powered by LinuxGSM",
 	"Overengineering addons",
-	"64 char hostname limit"
+	"63charhostnamelimitwhy"
 }
 
 -- Verify name lengths
 for _, name in ipairs(names) do
 	local length = #rootName:GetString() + #name
-	if length > 64 then
+	if length > 63 then
 		print(string.format("WARNING | Name %s is %i longer than the maximum number of characters allowed", name, length - 64))
 	end
 end
