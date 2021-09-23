0ab1874575252dba131ad60b97681b0ad18f24a4
Derpius <49565664+Derpius@users.noreply.github.com>
2021-09-23 17:10:02 +0100

    Basic join and leave messages

--- lua/sv-tas-utils/joinsleaves.lua
+++ lua/sv-tas-utils/joinsleaves.lua
@@ -1,11 +1,13 @@
-hook.Add("PlayerConnect", "TAS.JoinAndLeaveMsgs", function(plr)
-	-- Print connecting message
+local fontColour = Color(255, 152, 79)
+
+hook.Add("PlayerConnect", "TAS.JoinAndLeaveMsgs", function(name)
+	TASUtils.ChatPrint(fontColour, string.format("%s is connecting to the server...", name))
 end)
 
 hook.Add("PlayerSpawn", "TAS.JoinAndLeaveMsgs", function(plr)
-	-- Print join message
+	TASUtils.ChatPrint(team.GetColor(plr:Team()), plr:Nick(), fontColour, " just joined the server")
 end)
 
 hook.Add("PlayerDisconnected", "TAS.JoinAndLeaveMsgs", function(plr)
-	-- Print leave message here
+	TASUtils.ChatPrint(team.GetColor(plr:Team()), plr:Nick(), fontColour, " left the server")
 end)
