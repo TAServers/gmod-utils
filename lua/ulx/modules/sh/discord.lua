ae4d1fe6e9453485c6be8d8cf2ce4603742cc6c7
Derpius <49565664+Derpius@users.noreply.github.com>
2021-09-24 13:56:42 +0100

    Add `!discord`

---
+++ lua/ulx/modules/sh/discord.lua
@@ -0,0 +1,6 @@
+local cmd = ulx.command("TAS Utilities", "ulx discord", function(plr)
+	if not IsValid(plr) then return end
+	plr:ChatPrint("discord.gg/XqxhqzS72R")
+end, "!discord")
+cmd:defaultAccess(ULib.ACCESS_ALL)
+cmd:help("Prints the Discord server invite")
