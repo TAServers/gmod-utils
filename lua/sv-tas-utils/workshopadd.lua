05826f30d919e439301fbb66d65f8f3fa138c486
puypt <puypt@blahblah.com>
2021-09-25 12:14:21 -0700

    Add some gun addons

--- lua/sv-tas-utils/workshopadd.lua
+++ lua/sv-tas-utils/workshopadd.lua
@@ -1,7 +1,14 @@
 -- Adds addon(s) to the workshop download queue (since they dont do it themselves)
 
 local addons = {
-    "104990330" -- WAC AIRCRAFT ONLY!!
+    "104990330", -- WAC AIRCRAFT ONLY!!
+    -- TFA Insurgency --
+    "866368346",
+    "873701799",
+    "878962980",
+    "1490124474",
+    "1676032134",
+    "1429966932"
 }
 
 for _, id in pairs(addons) do
