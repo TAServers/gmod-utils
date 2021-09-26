58ce6bb6f6a77b760ccd0ba2ab682df4b87df4a9
Derpius <49565664+Derpius@users.noreply.github.com>
2021-09-26 22:07:18 +0100

    Implement label

--- html/components/circularprogress/script.js
+++ html/components/circularprogress/script.js
@@ -1,5 +1,15 @@
 var CACHED_CIRCUMFERENCE = 188.495559215;
 
+function setLabelPos(label, percentage, radius) {
+	var pi2pct = Math.PI * 2 * percentage;
+
+	var x = Math.cos(pi2pct) * radius + 50;
+	var y = Math.sin(pi2pct) * radius + 50;
+
+	label.setAttribute("x", x.toString());
+	label.setAttribute("y", y.toString());
+}
+
 function createProgressBar(title, parent) {
 	var barContainer = document.createElement("object");
 	barContainer.setAttribute("type", "image/svg+xml");
@@ -8,6 +18,7 @@ function createProgressBar(title, parent) {
 	barContainer.bars = {};
 
 	barContainer.addElement = function(id, colour) {
+		// Progress bar
 		var bar = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 		bar.setAttribute("class", "bar");
 		bar.setAttribute("r", "30");
@@ -21,6 +32,17 @@ function createProgressBar(title, parent) {
 		bar.setAttribute("stroke-dashoffset", "0");
 		bar.setAttribute("stroke", "rgb(" + colour[0].toString() + "," + colour[1].toString() + "," + colour[2].toString() + ")");
 
+		// Label
+		var label = document.createElementNS("http://www.w3.org/2000/svg", "text");
+		label.setAttribute("class", "label");
+		label.setAttribute("text-anchor", "middle");
+		label.setAttribute("dominant-baseline", "middle");
+		setLabelPos(label, 1, 30);
+
+		label.innerText = title;
+
+		bar.appendChild(label);
+
 		this.contentDocument.getElementsByTagName("svg")[0].appendChild(bar);
 		this.bars[id] = bar;
 	};
@@ -31,6 +53,10 @@ function createProgressBar(title, parent) {
 			(CACHED_CIRCUMFERENCE - percentage * CACHED_CIRCUMFERENCE).toString()
 		);
 	}
+
+	barContainer.addEventListener("load", function() {
+		barContainer.contentDocument.getElementsByClassName("title")[0].innerText = title;
+	});
 
 	parent.appendChild(barContainer);
 	return barContainer;
