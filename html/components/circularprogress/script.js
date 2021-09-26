8eab39bfd6b993b0acdf192b6046900a90970ba9
Derpius <49565664+Derpius@users.noreply.github.com>
2021-09-26 20:15:57 +0100

    Testing OOP progress bars

---
+++ html/components/circularprogress/script.js
@@ -0,0 +1,38 @@
+var CACHED_CIRCUMFERENCE = 188.495559215;
+
+function createProgressBar(title, parent) {
+	var barContainer = document.createElement("object");
+	barContainer.setAttribute("type", "image/svg+xml");
+	barContainer.setAttribute("data", "circularprogress.svg");
+
+	barContainer.bars = {};
+
+	barContainer.addElement = function(id, colour) {
+		var bar = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+		bar.setAttribute("class", "bar");
+		bar.setAttribute("r", "30");
+		bar.setAttribute("cx", "50");
+		bar.setAttribute("cy", "50");
+		bar.setAttribute("fill", "transparent");
+		bar.setAttribute("transform", "rotate(-90, 50, 50)");
+		bar.setAttribute("stroke-linecap", "round");
+
+		bar.setAttribute("stroke-dasharray", CACHED_CIRCUMFERENCE.toString());
+		bar.setAttribute("stroke-dashoffset", "0");
+		bar.setAttribute("stroke", "rgb(" + colour[0].toString() + "," + colour[1].toString() + "," + colour[2].toString() + ")");
+
+		console.log(this.contentDocument);
+		this.contentDocument.getElementsByTagName("svg")[0].appendChild(bar);
+		this.bars[id] = bar;
+	};
+
+	barContainer.setProgress = function(id, percentage) {
+		this.bars[id].setAttribute(
+			"stroke-dashoffset",
+			(CACHED_CIRCUMFERENCE - percentage * CACHED_CIRCUMFERENCE).toString()
+		);
+	}
+
+	parent.appendChild(barContainer);
+	return barContainer;
+}
