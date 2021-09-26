3ceb5ecf767edf768285c110a08fa86f3fbbedd5
Derpius <49565664+Derpius@users.noreply.github.com>
2021-09-26 22:17:58 +0100

    Use correct tag

--- html/components/circularprogress/script.js
+++ html/components/circularprogress/script.js
@@ -1,7 +1,7 @@
 var CACHED_CIRCUMFERENCE = 188.495559215;
 
 function setLabelPos(label, percentage, radius) {
-	var pi2pct = Math.PI * 2 * percentage;
+	var pi2pct = Math.PI * 2 * percentage - Math.PI / 2;
 
 	var x = Math.cos(pi2pct) * radius + 50;
 	var y = Math.sin(pi2pct) * radius + 50;
@@ -19,16 +19,18 @@ function createProgressBar(title, parent) {
 
 	barContainer.addElement = function(id, colour) {
 		// Container
-		var container = document.createElement("svg");
+		var container = document.createElement("http://www.w3.org/2000/svg", "g");
 
 		// Progress bar
 		var bar = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 		bar.setAttribute("class", "bar");
+
 		bar.setAttribute("r", "30");
 		bar.setAttribute("cx", "50");
 		bar.setAttribute("cy", "50");
-		bar.setAttribute("fill", "transparent");
 		bar.setAttribute("transform", "rotate(-90, 50, 50)");
+
+		bar.setAttribute("fill", "transparent");
 		bar.setAttribute("stroke-linecap", "round");
 
 		bar.setAttribute("stroke-dasharray", CACHED_CIRCUMFERENCE.toString());
