50d87b0784b16542694727fba7e5c3f1704752aa
Derpius <49565664+Derpius@users.noreply.github.com>
2021-09-26 20:25:26 +0100

    Remove logging and set attribute instead of css

--- html/components/circularprogress/script.js
+++ html/components/circularprogress/script.js
@@ -19,9 +19,8 @@ function createProgressBar(title, parent) {
 
 		bar.setAttribute("stroke-dasharray", CACHED_CIRCUMFERENCE.toString());
 		bar.setAttribute("stroke-dashoffset", "0");
-		bar.style.stroke = "rgb(" + colour[0].toString() + "," + colour[1].toString() + "," + colour[2].toString() + ");";
+		bar.setAttribute("stroke", "rgb(" + colour[0].toString() + "," + colour[1].toString() + "," + colour[2].toString() + ")");
 
-		console.log(this.contentDocument);
 		this.contentDocument.getElementsByTagName("svg")[0].appendChild(bar);
 		this.bars[id] = bar;
 	};
