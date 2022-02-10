00a8b979cea44dcf5279d7d2c1dfbdeee18bbf3a
Derpius <49565664+Derpius@users.noreply.github.com>
2022-02-10 00:15:15 +0000

    Add map persistent server monitors

---
+++ lua/sv-tas-utils/initpostentity/mapsvmonitors.lua
@@ -0,0 +1,15 @@
+local MAP_LOCATIONS = {
+	gm_bigcity = {
+		{Vector(-512.656250, 1631.562500, -11080.437500), Angle(0, 0, 0)}
+	}
+}
+
+local curMapLocs = MAP_LOCATIONS[game.GetMap()]
+if curMapLocs and #curMapLocs > 0 then
+	for _, location in ipairs(curMapLocs) do
+		local monitor = ents.Create("servermonitor")
+		monitor:SetPos(location[1])
+		monitor:SetAngles(location[2])
+		monitor:Spawn()
+	end
+end
