f8526663c7e02d50d04bf0a72e889b469572ee0c
Derpius <49565664+Derpius@users.noreply.github.com>
2022-02-10 00:39:56 +0000

    Add initial construct monitor location

--- lua/sv-tas-utils/initpostentity/mapsvmonitors.lua
+++ lua/sv-tas-utils/initpostentity/mapsvmonitors.lua
@@ -1,6 +1,9 @@
 local MAP_LOCATIONS = {
 	gm_bigcity = {
 		{Vector(-512.656250, 1631.562500, -11080.437500), Angle(90, 180, 180)}
+	},
+	gm_construct = {
+		{Vector(1024.375000, -126.218750, -83.250000), Angle(90, 0, 180)}
 	}
 }
 
