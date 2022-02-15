66daf11744f2ea3f9c117d9b9e55ec275f664984
Derpius <49565664+Derpius@users.noreply.github.com>
2022-02-15 16:52:27 +0000

    Basically sir papate has shit code

--- lua/sv-tas-utils/plycoreperms.lua
+++ lua/sv-tas-utils/plycoreperms.lua
@@ -1,18 +1,24 @@
 local BLACKLIST = {
-	plyGod = true
+	god = true,
+	getip = true
 }
 
 local BUILDMODE_ONLY = {
-	plyApplyForce = true,
-	plySetPos = true,
-	--plySetAng = true, Allowed for writing aimbots
-	plyNoclip = true,
-	plySetHealth = true,
-	plySetArmor = true,
-	plySetSpeed = true
+	applyforce = true,
+	setpos = true,
+	--setang = true, Allowed for aimbots
+	noclip = true,
+	sethealth = true,
+	setarmor = true,
+	setspeed = true,
+	setrunspeed = true,
+	setwalkspeed = true,
+	ignite = true
 }
 
+hook.GetTable().PlyCoreCommand.ULX_PlyCore_Access = nil
 hook.Add("PlyCoreCommand", "TASUtils.PlyCorePerms", function(caller, target, command)
+	print("TEST")
 	if BLACKLIST[command] then return true end
 	if BUILDMODE_ONLY[command] and (not caller:HasBuildMode() or not target:HasBuildMode()) then return true end
 end)
