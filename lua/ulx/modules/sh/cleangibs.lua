80a77b31c39b94b5f775ae99bc17a9c6f589ad49
zubbbz <zubbbz@noreply.taservers.com>
2022-02-16 16:59:11 +0000

    add chatlog for when this is used

--- lua/ulx/modules/sh/cleangibs.lua
+++ lua/ulx/modules/sh/cleangibs.lua
@@ -2,6 +2,7 @@ local cleanGibsCmd = ulx.command(TASUtils.Category, "ulx cleangibs", function()
     for _, ent in pairs(ents.FindByClass("gib")) do
         SafeRemoveEntity(ent)
     end
+    ulx.fancyLogAdmin(ply, "#A cleaned up gibs")
 end, "!cleangibs")
 
 cleanGibsCmd:defaultAccess(ULib.ACCESS_ADMIN)
