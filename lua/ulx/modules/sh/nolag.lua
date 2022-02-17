4853dd6e30381f365ff85ac6f1a1bf36fcea44f1
Derpius <49565664+Derpius@users.noreply.github.com>
2022-02-17 04:45:09 +0000

    Fix nolag

--- lua/ulx/modules/sh/nolag.lua
+++ lua/ulx/modules/sh/nolag.lua
@@ -1,14 +1,14 @@
 -- Freezes all non-world entities, or entities from a specific target
 ---@param caller Player Person who called this command
----@param target? Player Optional player to freeze props of
-local function nolagLogic(caller, targets)
+---@param targets? Player[] Players to freeze props of
+local function nolag(caller, targets)
     if not NADMOD then return end -- We should always have NADMOD installed, so the sanity check just needs to return
 
     local targetLut = {}
-    for _, plr in targets do targetLut[plr] = true end
+    for _, plr in ipairs(targets) do targetLut[plr] = true end
 
     local count = 0
-    for _, prop in ipairs(NADMOD) do
+    for _, prop in pairs(NADMOD.Props) do
         if prop.Owner ~= game.GetWorld() and (not targets or targetLut[prop.Owner]) and IsValid(prop.Ent) then
             local phys = prop.Ent:GetPhysicsObject()
             if IsValid(phys) then
@@ -18,16 +18,15 @@ local function nolagLogic(caller, targets)
         end
     end
 
-    local msg = target and "#A froze %u prop%s owned by #T" or "#A froze %u prop%s"
     local suffix = count == 1 and "" or "s"
-    ulx.fancyLogAdmin(caller, msg:format(count, suffix), target)
+    ulx.fancyLogAdmin(caller, ("#A froze %u prop%s owned by #T"):format(count, suffix), targets)
 end
 
-local nolagCmd = ulx.command(TASUtils.Category, "ulx nolag", nolagLogic, "!nolag")
-nolagCmd:defaultAccess(ULib.ACCESS_ADMIN)
-nolagCmd:help("Freezes entities on the server")
-nolagCmd:addParam({
-	type = ULib.cmds.PlayerArg,
+local cmd = ulx.command(TASUtils.Category, "ulx nolag", nolag, "!nolag")
+cmd:defaultAccess(ULib.ACCESS_ADMIN)
+cmd:help("Freezes entities owned by players")
+cmd:addParam({
+	type = ULib.cmds.PlayersArg,
 	default = "*",
 	ULib.cmds.optional
 })
