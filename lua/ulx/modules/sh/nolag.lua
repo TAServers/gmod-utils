2f85dd4809f8370ec56334b66d9a16fefe0b7353
zubbbz <zubbbz@noreply.taservers.com>
2022-02-17 04:10:17 +0000

    I'm a tardass

--- lua/ulx/modules/sh/nolag.lua
+++ lua/ulx/modules/sh/nolag.lua
@@ -1,7 +1,7 @@
 -- Freezes all non-world entities, or entities from a specific target
 ---@param caller Player Person who called this command
 ---@param target? Player Optional player to freeze props of
-local function nolag(caller, targets)
+local function nolagLogic(caller, targets)
     if not NADMOD then return end -- We should always have NADMOD installed, so the sanity check just needs to return
 
     local targetLut = {}
@@ -22,3 +22,12 @@ local function nolag(caller, targets)
     local suffix = count == 1 and "" or "s"
     ulx.fancyLogAdmin(caller, msg:format(count, suffix), target)
 end
+
+local nolagCmd = ulx.command(TASUtils.Category, "ulx nolag", nolagLogic, "!nolag")
+nolagCmd:defaultAccess(ULib.ACCESS_ADMIN)
+nolagCmd:help("Freezes entities on the server")
+nolagCmd:addParam({
+	type = ULib.cmds.PlayerArg,
+	default = "*",
+	ULib.cmds.optional
+})
