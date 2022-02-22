337f4b7427a26b56e479f9f0e96a1fc9bee073c1
puyamog <puyamog@gmail.com>
2022-02-21 18:52:07 -0800

    Some major optimizations to double jumping

--- lua/sv-tas-utils/doublejump.lua
+++ lua/sv-tas-utils/doublejump.lua
@@ -1,84 +1,26 @@
-local STATE_JUMPED = 0
-local STATE_LANDED = 1
--- How far the player needs to be from the ground to double jump
--- This is primarily so people who walk off ledges don't accidentally jump
-local DIST_FROM_GROUND = 20
--- How long it has to be before a player may double jump
+-- How long it has to be before a player may double jump, this helps immediate double jump issues
 local DOUBLE_JUMP_TIME = 0.03
 -- How much to add forward velocity (adds much more of a "leap" feel)
 local FORWARD_INFLUENCE = 64
 -- How much more power to let the double jump have
 local POWER_INCREASE = 1.5
 
+-- Make sure clients also get our effect
 AddCSLuaFile("effects/doublejump.lua")
 
 -- The star of the script
 local function onDoubleJump(ply)
+	-- This sends the plater primarily flying upwards, but also adds some forward velocity
 	local doubleJumpPower = ply:GetJumpPower() * ply:GetPhysicsObject():GetMass() * POWER_INCREASE
 	local forwardVector = ply:GetAimVector() * FORWARD_INFLUENCE
 
 	ply:SetVelocity(Vector(0, 0, doubleJumpPower) + forwardVector)
 
+	-- Play a little effect when we jump
 	local fxData = EffectData()
 	fxData:SetOrigin(ply:GetPos())
 	fxData:SetEntity(ply)
 	util.Effect("doublejump", fxData, true, true)
-end
-
--- Player's current jump/land status
-local states = {}
-
--- Changes the state of a player
-local function changeState(ply, state)
-	assert(IsValid(ply) and ply, "Player is not valid")
-
-	states[ply] = state
-	if state == STATE_JUMPED then
-		ply.lastJump = CurTime()
-	end
-end
-
--- Retrieves the lazily loaded state of a player
-local function getState(ply)
-	assert(IsValid(ply) and ply, "Player is not valid")
-	if not states[ply] then
-		changeState(ply, STATE_LANDED)
-	end
-
-	return states[ply]
-end
-
--- Returns whether a player is on the ground or not
-local function onGround(ply)
-	assert(IsValid(ply) and ply, "Player is not valid")
-	local trace = {
-		start = ply:GetPos(),
-		endpos = ply:GetPos() + Vector(0, 0, -DIST_FROM_GROUND),
-		filter = ply
-	}
-
-	local result = util.TraceLine(trace)
-	return result.Hit
-end
-
--- Updates every player's jump status
--- NOTE: This also checks if they want to jump - but it's not exactly a reliable method
--- we have a reliable method that is the one that primarily checks if they want to jump (and double jump!)
-local function updateStates()
-	for _, ply in pairs(player.GetAll()) do
-		local state = getState(ply)
-
-		if state == STATE_LANDED and not onGround(ply) then
-			-- Player is reporting they've landed, but they're not actually on the ground
-			-- aka: they jumped
-			changeState(ply, STATE_JUMPED)
-		elseif state == STATE_JUMPED and onGround(ply) then
-			-- Player is reporting they've jumped, but they're actually on the ground
-			-- aka: they landed
-			changeState(ply, STATE_LANDED)
-			ply.doubleJumped = false -- Reset the double jump status 
-		end
-	end
 end
 
 -- Analyzes the CMoveData and determines if the player wants to jump
@@ -88,12 +30,8 @@ local function onMove(ply, data)
 	-- the keyPressed variable works if someone rebinds their jump key (who the fuck would do that)
 
 	if keyPressed or velocityCheck then
-		local state = getState(ply)
-
-		if state == STATE_LANDED then
-			-- Player is on the ground and wants to jump
-			changeState(ply, STATE_JUMPED)
-		elseif state == STATE_JUMPED and not ply.doubleJumped then
+		if not ply:OnGround() and not ply.doubleJumped then
+			-- Without this, the double jump would occur instantly
 			local duration = CurTime() - ply.lastJump
 			if duration < DOUBLE_JUMP_TIME then
 				return 
@@ -102,9 +40,15 @@ local function onMove(ply, data)
 			-- They're mid-air, so invoke the double jump
 			ply.doubleJumped = true 
 			onDoubleJump(ply)
+		else
+			-- If this is a normal jump, record it
+			ply.lastJump = CurTime()
 		end
 	end
 end
 
-hook.Add("Think", "TASUtils.DoubleJumpUpdate", updateStates)
 hook.Add("Move", "TASUtils.DoubleJumpMove", onMove)
+hook.Add("OnPlayerHitGround", "TASUtils.DoubleJumpReset", function(ply)
+	-- Simply reset their double jump flag, not enough to move it into its own function
+	ply.doubleJumped = false
+end)
