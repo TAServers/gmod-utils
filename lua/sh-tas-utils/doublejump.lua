982782d17c036dd6dc06cbcefbab6129a9d9f690
puyamog <puyamog@gmail.com>
2022-02-21 19:39:31 -0800

    Simpler implementation with KeyPress

--- lua/sh-tas-utils/doublejump.lua
+++ lua/sh-tas-utils/doublejump.lua
@@ -1,7 +1,7 @@
 -- How long it has to be before a player may double jump, this helps immediate double jump issues
 local DOUBLE_JUMP_TIME = 0.03
 -- How much to add forward velocity (adds much more of a "leap" feel)
-local FORWARD_INFLUENCE = 64
+local FORWARD_INFLUENCE = 256
 -- How much more power to let the double jump have
 local POWER_INCREASE = 1.5
 
@@ -11,43 +11,43 @@ AddCSLuaFile("effects/doublejump.lua")
 -- The star of the script
 local function onDoubleJump(ply)
 	-- This sends the plater primarily flying upwards, but also adds some forward velocity
-	local doubleJumpPower = ply:GetJumpPower() * ply:GetPhysicsObject():GetMass() * POWER_INCREASE
+	local doubleJumpPower = ply:GetJumpPower() * POWER_INCREASE
 	local forwardVector = ply:GetAimVector() * FORWARD_INFLUENCE
 
 	ply:SetVelocity(Vector(0, 0, doubleJumpPower) + forwardVector)
 
 	-- Play a little effect when we jump
+	-- We also still need this on server even on shared, because this will replicate other players' effects
 	local fxData = EffectData()
 	fxData:SetOrigin(ply:GetPos())
 	fxData:SetEntity(ply)
-	util.Effect("doublejump", fxData, true, true)
+	local recipients = nil 
+
+	if SERVER then
+		recipients = RecipientFilter()
+		recipients:AddAllPlayers()
+		recipients:RemovePlayer(ply) -- Remove ourselves since we do it on the client
+	end
+	
+	util.Effect("doublejump", fxData, true, recipients)
 end
 
--- Analyzes the CMoveData and determines if the player wants to jump
-local function onMove(ply, data)
-	local keyPressed = data:KeyPressed(IN_JUMP)
-	local velocityCheck = data:GetFinalJumpVelocity().z > 0 -- This is here because I dont know if
-	-- the keyPressed variable works if someone rebinds their jump key (who the fuck would do that)
+local function onKeyPress(ply, btn)
+	-- If we jump, check if its a double jump
+	if btn == IN_JUMP then
+		local isDoubleJump = not ply:OnGround() and not ply.doubleJumped 
 
-	if keyPressed or velocityCheck then
-		if not ply:OnGround() and not ply.doubleJumped then
-			-- Without this, the double jump would occur instantly
-			local duration = CurTime() - ply.lastJump
-			if duration < DOUBLE_JUMP_TIME then
-				return 
-			end
-
-			-- They're mid-air, so invoke the double jump
-			ply.doubleJumped = true 
+		if isDoubleJump then
+			ply.doubleJumped = true
 			onDoubleJump(ply)
 		else
-			-- If this is a normal jump, record it
+			-- Normal jump, record it
 			ply.lastJump = CurTime()
 		end
 	end
 end
 
-hook.Add("Move", "TASUtils.DoubleJumpMove", onMove)
+hook.Add("KeyPress", "TASUtils.DoubleJumpDetect", onKeyPress)
 hook.Add("OnPlayerHitGround", "TASUtils.DoubleJumpReset", function(ply)
 	-- Simply reset their double jump flag, not enough to move it into its own function
 	ply.doubleJumped = false
