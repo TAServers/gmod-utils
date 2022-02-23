3746a39617eb1f685719331a04415daf49aceb20
puyamog <puyamog@gmail.com>
2022-02-22 19:37:20 -0800

    Resolve some review issues, better naming

--- lua/sh-tas-utils/doublejump.lua
+++ lua/sh-tas-utils/doublejump.lua
@@ -1,45 +1,43 @@
 -- How much to add forward velocity (adds much more of a "leap" feel)
-local FORWARD_INFLUENCE = 256
+local FORWARD_INFLUENCE = 180
 -- How much more power to let the double jump have
-local POWER_INCREASE = 1.5
+local POWER_INCREASE = 2
 
 -- Make sure clients also get our effect
 AddCSLuaFile("effects/doublejump.lua")
 
--- The star of the script
-local function onDoubleJump(ply, move)
-	-- This sends the player primarily flying upwards, but also adds some forward velocity
-	local doubleJumpPower = ply:GetJumpPower() * POWER_INCREASE
-	local forwardVector = ply:GetAimVector() * FORWARD_INFLUENCE
-
-	move:SetVelocity(move:GetVelocity() + Vector(0, 0, doubleJumpPower) + forwardVector)
-
-	-- Play a little effect when we jump
-	-- We also still need this on server even on shared, because this will replicate other players' effects
-	local fxData = EffectData()
-	fxData:SetOrigin(ply:GetPos())
-	fxData:SetEntity(ply)
-	local recipients = nil 
-
-	if SERVER then
-		recipients = RecipientFilter()
-		recipients:AddAllPlayers()
-		recipients:RemovePlayer(ply) -- Remove ourselves since we do it on the client
-	end
-	
-	util.Effect("doublejump", fxData, true, recipients)
-end
-
-local function onSetupMove(ply, move)
+hook.Add("SetupMove", "TASUtils.DoubleJump", function(ply, move)
 	-- Check if the move has a jump command, and check if its a double jump
 	if move:KeyPressed(IN_JUMP) and not ply:OnGround() and not ply.doubleJumped then
+		-- A double jump has been initiated
 		ply.doubleJumped = true 
-		onDoubleJump(ply, move)
-	end
-end
+		-- This sends the player primarily flying upwards, but also adds some forward velocity
+		local doubleJumpPower = ply:GetJumpPower() * POWER_INCREASE
+		local forwardVector = ply:GetAimVector() * FORWARD_INFLUENCE
 
-hook.Add("SetupMove", "TASUtils.DoubleJumpDetect", onSetupMove)
-hook.Add("OnPlayerHitGround", "TASUtils.DoubleJumpReset", function(ply)
+		-- Set the velocity of the double jump
+		move:SetVelocity(move:GetVelocity() + Vector(0, 0, doubleJumpPower) + forwardVector)
+
+		-- Play a little effect when we jump
+		-- We also still need this on server even on shared, because this will replicate other players' effects
+		local fxData = EffectData()
+		fxData:SetOrigin(ply:GetPos())
+		fxData:SetEntity(ply)
+		local recipients = nil 
+
+		-- Without this here - the client would play an effect, and the server would too - this
+		-- prevents double effects
+		if SERVER then
+			recipients = RecipientFilter()
+			recipients:AddAllPlayers()
+			recipients:RemovePlayer(ply) -- Remove ourselves since we do it on the client
+		end
+		
+		util.Effect("doublejump", fxData, true, recipients)
+	end
+end)
+
+hook.Add("OnPlayerHitGround", "TASUtils.DoubleJump", function(ply)
 	-- Simply reset their double jump flag, not enough to move it into its own function
 	ply.doubleJumped = false
 end)
