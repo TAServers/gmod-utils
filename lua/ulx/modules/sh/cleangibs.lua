c663079f8072a114d30b23718d57ddc1d441ef7e
zubbbz <zubbbz@noreply.taservers.com>
2022-03-06 10:19:39 +0000

    idk i fuckin broke it last commit

--- lua/ulx/modules/sh/cleangibs.lua
+++ lua/ulx/modules/sh/cleangibs.lua
@@ -1,10 +1,12 @@
-local TYPES = {"gib","item_*","debris","helicopter_chunk","weapon_*","prop_combine_ball","prop_ragdoll"}
+local TYPES = {"gib","item_*","debris","helicopter_chunk","weapon_*","prop_combine_ball"}
 local Count = 0
+
+-- dead npc's dont have ondie functions
 
 local cleanGibsCmd = ulx.command(TASUtils.Category, "ulx cleangibs", function(calling_ply)
     for _, class in ipairs(TYPES) do
         for _, ent in ipairs(ents.FindByClass(class)) do
-            if ent:GetOwner() ~= nil then
+            if ent:IsValid() and ent:GetOwner() ~= NULL then -- i understand null is undefined, but it works :person_shrugging:
                 SafeRemoveEntity(ent)
                 Count = Count + 1
             end
