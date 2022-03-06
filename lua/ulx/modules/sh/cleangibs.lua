60784340daaf7acee8e49dff579e1492cd0c3dfd
zubbbz <zubbbz@noreply.taservers.com>
2022-03-06 05:25:20 +0000

    improve clean gibs command
    cleans more gibs, counts them, some code refactored from E2bi :trollge:

--- lua/ulx/modules/sh/cleangibs.lua
+++ lua/ulx/modules/sh/cleangibs.lua
@@ -1,8 +1,14 @@
+local TYPES = {"gib","item_*","debris","helicopter_chunk"}
+local Count = 0
+
 local cleanGibsCmd = ulx.command(TASUtils.Category, "ulx cleangibs", function(calling_ply)
-    for _, ent in pairs(ents.FindByClass("gib")) do
-        SafeRemoveEntity(ent)
+    for _, class in ipairs(TYPES) do
+        for _, ent in ipairs(ents.FindByClass(class)) do
+            SafeRemoveEntity(ent)
+            Count = Count + 1
+        end
     end
-    ulx.fancyLogAdmin(calling_ply, "#A cleaned up gibs")
+    ulx.fancyLogAdmin(calling_ply, "#A cleaned up #i gibs", Count)
 end, "!cleangibs")
 
 cleanGibsCmd:defaultAccess(ULib.ACCESS_ADMIN)
