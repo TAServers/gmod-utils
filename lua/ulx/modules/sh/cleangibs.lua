342a8e84cce378f7d7cdecf4faf1d26dda64c72a
zubbbz <zubbbz@noreply.taservers.com>
2022-03-07 03:00:15 +0000

    multiple fixed :tm:
    fixed counter thingy
    fixed something derpius complained about (thanks for the help)

--- lua/ulx/modules/sh/cleangibs.lua
+++ lua/ulx/modules/sh/cleangibs.lua
@@ -1,18 +1,17 @@
 local TYPES = {"gib","item_*","debris","helicopter_chunk","weapon_*","prop_combine_ball"}
-local Count = 0
-
--- dead npc's dont have ondie functions
+-- Currently we dont remove dead bodies, but a thing to note is dead npc's do not have ondie functions
 
 local cleanGibsCmd = ulx.command(TASUtils.Category, "ulx cleangibs", function(calling_ply)
+    local Count = 0
     for _, class in ipairs(TYPES) do
         for _, ent in ipairs(ents.FindByClass(class)) do
-            if ent:IsValid() and ent:GetOwner() == NULL then -- i understand null is undefined, but it works :person_shrugging:
+            if ent:IsValid() and not ent:GetOwner():IsValid() then
                 SafeRemoveEntity(ent)
                 Count = Count + 1
             end
         end
     end
-    ulx.fancyLogAdmin(calling_ply, "#A cleaned up #i gibs", Count)
+    ulx.fancyLogAdmin(calling_ply, "#A cleaned up #i gib(s)", Count)
 end, "!cleangibs")
 
 cleanGibsCmd:defaultAccess(ULib.ACCESS_ADMIN)
