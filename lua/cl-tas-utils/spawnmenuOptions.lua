eb4f2e3d5b4d2e673e58c7583891de0499f3ec6f
Zubbbz <37497090+Zubbbz@users.noreply.github.com>
2022-03-22 22:32:28 -0400

    multiple fixes  (i think i fixed the desync potato was talking about), fixed the initial value not being networked

--- lua/cl-tas-utils/spawnmenuOptions.lua
+++ lua/cl-tas-utils/spawnmenuOptions.lua
@@ -1,13 +1,24 @@
-local DoubleJumpEnabled = CreateConVar("doublejump_enabled", 1, FCVAR_ARCHIVE, "", 0, 1)
+local DoubleJumpEnabled = CreateConVar("doublejump_enabled", 1, FCVAR_ARCHIVE, "toggle doublejumping", 0, 1)
+
+local function NetDJ(bool) -- i just made it a function for convenience
+    net.Start("TASUtils.DoubleJump")
+    net.WriteBool(bool)
+    net.SendToServer()
+end
+
+hook.Add("InitPostEntity", "initial netmsg", function()
+    NetDJ(DoubleJumpEnabled:GetBool())
+end)
 
 -- options menu drawing
-local function OptionsMenu(optionsPanel)
+local function OptionsMenu(optionsPanel) -- called on join when spawnmenu is initialized
     dj_checkbox = vgui.Create("DCheckBoxLabel")
     dj_checkbox:SetText("Toggle Double Jump")
-    dj_checkbox:SetTextColor(Color(0, 0, 0))
-    dj_checkbox:AlignLeft(30)
+    dj_checkbox:SetTextColor(Color(0, 0, 0)) -- for some reason without this the text doesn't look correct :xok:
+    dj_checkbox:AlignLeft(30) -- maybe try to use this for any future options we add to make it look consistent and not weird
     dj_checkbox:SetChecked(DoubleJumpEnabled:GetBool())
     optionsPanel:AddItem(dj_checkbox)
+
     function dj_checkbox:OnChange(val)
         DoubleJumpEnabled:SetBool(val)
     end
@@ -15,13 +26,10 @@ local function OptionsMenu(optionsPanel)
     -- as well as the color and thickness of the halo that appears around players in build mode
 end
 
--- if someone changes the convar with console, then update the box
+-- if the convar is changed, update the box 
 cvars.AddChangeCallback("doublejump_enabled", function(name, old, new)
     dj_checkbox:SetChecked(new)
-
-    net.Start("TASUtils.DoubleJump")
-    net.WriteBool(DoubleJumpEnabled:GetBool())
-    net.SendToServer()
+    NetDJ(DoubleJumpEnabled:GetBool())
 end)
 
 hook.Add( "PopulateToolMenu", "AddTASOptionsCategory", function()
