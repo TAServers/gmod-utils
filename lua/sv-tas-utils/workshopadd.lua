4c7be6873e640ba56e1e701460dc9e8261962eb0
Derpius <49565664+Derpius@users.noreply.github.com>
2022-03-28 20:17:59 +0100

    Prune workshop adds

--- lua/sv-tas-utils/workshopadd.lua
+++ lua/sv-tas-utils/workshopadd.lua
@@ -1,23 +1,15 @@
 -- Adds addon(s) to the workshop download queue (since they dont do it themselves)
 
 local addons = {
-    "104990330", -- WAC AIRCRAFT ONLY!!
-    -- TFA Insurgency --
-    "866368346",
-    "873701799",
-    "878962980",
-    "1490124474",
-    "1676032134",
-    "1429966932",
-    "1482030308",
-    "866368352", -- The only sniper rifle..
-    "860057535",
-    "1231123967",
-    "858829873",
-    "1331399471",
-    "1525218777", -- this comma was sacred you know
     "415143062", -- TFA Base
-    "866368346" -- TFA Ins Shared Parts
+	"870165339", -- VFire
+
+    -- TFA Insurgency --
+    "866368346",  -- Shared Parts
+	"1676032134", -- AR-15
+	"1304932254", -- Beretta M9
+	"1243621966", -- H&K MP5K
+	"870165339"   -- Hyper's RFB
 }
 
 for _, id in pairs(addons) do
