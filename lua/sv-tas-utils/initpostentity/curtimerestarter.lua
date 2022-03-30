7632d6e13955dbdf4bb72e5473e51c8ec50646d7
Derpius <49565664+Derpius@users.noreply.github.com>
2022-03-30 04:54:48 +0100

    Restart based on how long the server's been empty

--- lua/sv-tas-utils/initpostentity/curtimerestarter.lua
+++ lua/sv-tas-utils/initpostentity/curtimerestarter.lua
@@ -1,8 +1,22 @@
-local curtimeRestartThresh = CreateConVar("curtime_restart_thresh", 43200, FCVAR_ARCHIVE, "CurTime at which to attempt to restart the server", 3600)
-hook.Add("Think", "TAS.CurTimeRestart", function()
-	if CurTime() > curtimeRestartThresh:GetInt() and player.GetCount() == 0 then
-		DiscordRelay.CachePost({type = "custom", body = "Restarting to reduce CurTime..."})
-		RunConsoleCommand("relay_stop")
-		RunConsoleCommand("changelevel", game.GetMap())
+local restartTime = CreateConVar(
+	"tasutils_time_to_restart", 60 * 30,
+	FCVAR_ARCHIVE,
+	"How long to wait after the server is empty to restart (seconds)",
+	600
+)
+
+hook.Add("PlayerDisconnected", "TAS.Restart", function()
+	if player.GetCount() == 0 then
+		timer.Create("TAS.Restart", restartTime:GetInt(), 1, function()
+			DiscordRelay.CachePost({type = "custom", body = "Resetting the server..."})
+			RunConsoleCommand("relay_stop")
+			RunConsoleCommand("changelevel", game.GetMap())
+		end)
+	end
+end)
+
+hook.Add("PlayerConnect", "TAS.Restart", function()
+	if timer.Exists("TAS.Restart") then
+		timer.Remove("TAS.Restart")
 	end
 end)
