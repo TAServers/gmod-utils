ac8562109699a7fe6922c0b0309287d523f45c98
Derpius <49565664+Derpius@users.noreply.github.com>
2023-01-08 11:42:13 +0000

    Remove global caching from chatprint

--- lua/sh-tas-utils/betterchatprint.lua
+++ lua/sh-tas-utils/betterchatprint.lua
@@ -1,57 +1,39 @@
 if SERVER then
 	util.AddNetworkString("TASUtils.ChatPrint")
 
-	-- Global caching
-	local _ipairs = ipairs
-	local string_char, _tostring = string.char, tostring
-	local _IsColor = IsColor
-	local util_Compress = util.Compress
-
-	local net_Start, net_WriteData, net_Send, net_Broadcast =
-		net.Start, net.WriteData, net.Send, net.Broadcast
-
 	local function encodeMsgArgs(...)
 		local header = ""
 		local body = ""
-		for _, v in _ipairs({ ... }) do
-			if _IsColor(v) then
+		for _, v in ipairs({ ... }) do
+			if IsColor(v) then
 				header = header .. "c"
-				body = body .. string_char(v:Unpack())
+				body = body .. string.char(v:Unpack())
 			else
 				header = header .. "s"
-				body = body .. _tostring(v) .. "\0"
+				body = body .. tostring(v) .. "\0"
 			end
 		end
 
-		return util_Compress(header .. "\0" .. body)
+		return util.Compress(header .. "\0" .. body)
 	end
 
 	-- Takes a vararg of colours and things to print (each item must have a valid tostring metamethod present)
 	function TASUtils.Broadcast(...)
-		net_Start("TASUtils.ChatPrint")
-		net_WriteData(encodeMsgArgs(...))
-		net_Broadcast()
+		net.Start("TASUtils.ChatPrint")
+		net.WriteData(encodeMsgArgs(...))
+		net.Broadcast()
 	end
 
 	FindMetaTable("Player").ChatPrint = function(self, ...)
-		net_Start("TASUtils.ChatPrint")
-		net_WriteData(encodeMsgArgs(...))
-		net_Send(self)
+		net.Start("TASUtils.ChatPrint")
+		net.WriteData(encodeMsgArgs(...))
+		net.Send(self)
 	end
 else
-	-- Global caching
-	local util_Decompress = util.Decompress
-	local net_ReadData = net.ReadData
-	local _error, _unpack = error, unpack
-	local table_insert = table.insert
-	local _Color = Color
-	local string_byte = string.byte
-	local chat_AddText = chat.AddText
-
 	net.Receive("TASUtils.ChatPrint", function(len)
-		local data = util_Decompress(net_ReadData(len))
+		local data = util.Decompress(net.ReadData(len))
 		if not data then
-			_error("Failed to decompress TASUtils.ChatPrint data")
+			error("Failed to decompress TASUtils.ChatPrint data")
 			return
 		end
 
@@ -75,9 +57,9 @@ else
 		local args = {}
 		for i = 1, headerLen do
 			if header[i] == "c" then -- Colours
-				table_insert(
+				table.insert(
 					args,
-					_Color(string_byte(data, dataPtr, dataPtr + 3))
+					Color(string.byte(data, dataPtr, dataPtr + 3))
 				)
 				dataPtr = dataPtr + 4 -- Skip over the three colour values
 			elseif header[i] == "s" then -- Strings
@@ -88,11 +70,11 @@ else
 				end
 				dataPtr = dataPtr + 1 -- Skip over null char
 
-				table_insert(args, substring)
+				table.insert(args, substring)
 			end
 		end
 
 		-- Display the parsed message
-		chat_AddText(_unpack(args))
+		chat.AddText(unpack(args))
 	end)
 end
