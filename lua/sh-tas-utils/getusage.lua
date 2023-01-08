1409b24883c968e00406e5978baa81b9183346bf
Derpius <49565664+Derpius@users.noreply.github.com>
2023-01-08 15:37:18 +0000

    Fix CPU usage calculations
    Remove limit on usage update

--- lua/sh-tas-utils/getusage.lua
+++ lua/sh-tas-utils/getusage.lua
@@ -21,14 +21,6 @@ local data = {
 	},
 }
 
-local updateRate = CreateConVar(
-	"tasutils_usage_updaterate",
-	8,
-	{ FCVAR_ARCHIVE, FCVAR_REPLICATED },
-	"How often to network usage data between realms (in ticks)",
-	1
-)
-
 if SERVER then
 	util.AddNetworkString("TASUtils.GetUsage")
 
@@ -39,13 +31,6 @@ if SERVER then
 		"Server desired tickrate used by TASUtils.GetUsage",
 		0
 	)
-	local svTpsMin = CreateConVar(
-		"tasutils_tickrate_min",
-		33,
-		FCVAR_ARCHIVE,
-		"Min point for server tickrate used by TASUtils.GetUsage",
-		0
-	)
 	local svRamMax = CreateConVar(
 		"tasutils_ram_max",
 		2,
@@ -54,7 +39,7 @@ if SERVER then
 		0.01
 	)
 
-	local function calcUsageSF(frameTime)
+	local function calcUsageSF(deltaTime)
 		if SF.playerInstances then
 			local total = 0
 			for _, instances in pairs(SF.playerInstances) do
@@ -63,7 +48,7 @@ if SERVER then
 				end
 			end
 
-			data.sv.sf.cpu = total / frameTime -- Starfall CPU usage measured as a percentage of the frame
+			data.sv.sf.cpu = total / deltaTime -- Starfall CPU usage measured as a percentage of the frame
 		end
 
 		if SF.Instance and SF.Instance.RamAvg then
@@ -72,7 +57,7 @@ if SERVER then
 	end
 
 	local e2RamMax
-	local function calcUsageE2(frameTime, ramUsage)
+	local function calcUsageE2(deltaTime, ramUsage)
 		if not e2RamMax then -- Wait for convar to exist
 			e2RamMax =
 				GetConVar("wire_expression2_ram_emergency_shutdown_total")
@@ -83,45 +68,43 @@ if SERVER then
 					total = total + e2.context.timebench
 				end
 			end
-			data.sv.e2.cpu = total / frameTime
+			data.sv.e2.cpu = total / deltaTime
 
 			data.sv.e2.ram = ramUsage / (e2RamMax:GetInt() * 1000) -- Yes this is the incorrect comparison, but it's what wire will terminate by so don't fix it until they do
 		end
 	end
 
-	local ticksSinceUpdate = updateRate:GetInt()
+	local lastFrameTime = SysTime()
 	hook.Add("Think", "TASUtils.GetUsage", function()
-		if ticksSinceUpdate >= updateRate:GetInt() then
-			local frameTime = FrameTime()
-			local ramUsage = collectgarbage("count")
+		local curFrameTime = SysTime()
+		local deltaTime = curFrameTime - lastFrameTime
+		lastFrameTime = curFrameTime
 
-			-- Update server usage
-			do
-				local min, max = svTpsMin:GetFloat(), svTpsMax:GetFloat()
-				data.sv.cpu =
-					math.max(1 - (1 / frameTime - min) / (max - min), 0)
-			end
-			data.sv.ram = ramUsage / (svRamMax:GetFloat() * 1024 * 1024)
+		local ramUsage = collectgarbage("count")
 
-			if E2Lib then
-				calcUsageE2(frameTime, ramUsage)
-			end
-
-			if SF then
-				calcUsageSF(frameTime)
-			end
-
-			net.Start("TASUtils.GetUsage")
-			net.WriteFloat(data.sv.cpu)
-			net.WriteFloat(data.sv.ram)
-			net.WriteFloat(data.sv.e2.cpu)
-			net.WriteFloat(data.sv.e2.ram)
-			net.WriteFloat(data.sv.sf.cpu)
-			net.WriteFloat(data.sv.sf.ram)
-			net.Broadcast()
-			ticksSinceUpdate = 0
+		-- Update server usage
+		do
+			local max = svTpsMax:GetFloat()
+			data.sv.cpu = math.max(1 - (1 / deltaTime) / max, 0)
 		end
-		ticksSinceUpdate = ticksSinceUpdate + 1
+		data.sv.ram = ramUsage / (svRamMax:GetFloat() * 1024 * 1024)
+
+		if E2Lib then
+			calcUsageE2(deltaTime, ramUsage)
+		end
+
+		if SF then
+			calcUsageSF(deltaTime)
+		end
+
+		net.Start("TASUtils.GetUsage")
+		net.WriteFloat(data.sv.cpu)
+		net.WriteFloat(data.sv.ram)
+		net.WriteFloat(data.sv.e2.cpu)
+		net.WriteFloat(data.sv.e2.ram)
+		net.WriteFloat(data.sv.sf.cpu)
+		net.WriteFloat(data.sv.sf.ram)
+		net.Broadcast()
 	end)
 else
 	net.Receive("TASUTils.GetUsage", function()
