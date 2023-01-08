fafcb2eb05a494731ae4985855c5b602340d0e5d
Derpius <49565664+Derpius@users.noreply.github.com>
2023-01-08 11:59:02 +0000

    Rewrite betterchatprint

--- lua/sh-tas-utils/betterchatprint.lua
+++ lua/sh-tas-utils/betterchatprint.lua
@@ -1,77 +1,48 @@
 if SERVER then
 	util.AddNetworkString("TASUtils.ChatPrint")
 
-	local function encodeMsgArgs(...)
-		local header = ""
-		local body = ""
-		for _, v in ipairs({ ... }) do
-			if IsColor(v) then
-				header = header .. "c"
-				body = body .. string.char(v:Unpack())
+	local function encodeMsg(args)
+		for _, v in ipairs(args) do
+			local isColor = IsColor(v)
+			net.WriteBool(isColor)
+
+			if isColor then
+				net.WriteColor(v, false)
 			else
-				header = header .. "s"
-				body = body .. tostring(v) .. "\0"
+				net.WriteString(tostring(v))
 			end
 		end
 
-		return util.Compress(header .. "\0" .. body)
+		if net.BytesWritten() > 1024 * 64 then
+			error("Tried to send too much data", 3)
+		end
 	end
 
 	-- Takes a vararg of colours and things to print (each item must have a valid tostring metamethod present)
 	function TASUtils.Broadcast(...)
+		local args = { ... }
+
 		net.Start("TASUtils.ChatPrint")
-		net.WriteData(encodeMsgArgs(...))
+		net.WriteUInt(#args, 16)
+		encodeMsg({ ... })
 		net.Broadcast()
 	end
 
 	FindMetaTable("Player").ChatPrint = function(self, ...)
+		local args = { ... }
+
 		net.Start("TASUtils.ChatPrint")
-		net.WriteData(encodeMsgArgs(...))
+		net.WriteUInt(#args, 16)
+		encodeMsg({ ... })
 		net.Send(self)
 	end
 else
-	net.Receive("TASUtils.ChatPrint", function(len)
-		local data = util.Decompress(net.ReadData(len))
-		if not data then
-			error("Failed to decompress TASUtils.ChatPrint data")
-			return
-		end
-
-		local dataPtr = 1
-
-		-- Parse header
-		local header, headerLen = "", 0
-		while true do
-			if data[dataPtr] == "\0" then
-				break
-			end
-
-			header = header .. data[dataPtr]
-			headerLen = headerLen + 1
-			dataPtr = dataPtr + 1
-		end
-
-		dataPtr = dataPtr + 1 -- Skip over the null char that caused the above loop to break
-
-		-- Parse body
+	net.Receive("TASUtils.ChatPrint", function()
 		local args = {}
-		for i = 1, headerLen do
-			if header[i] == "c" then -- Colours
-				table.insert(
-					args,
-					Color(string.byte(data, dataPtr, dataPtr + 3))
-				)
-				dataPtr = dataPtr + 4 -- Skip over the three colour values
-			elseif header[i] == "s" then -- Strings
-				local substring = ""
-				while data[dataPtr] ~= "\0" do
-					substring = substring .. data[dataPtr]
-					dataPtr = dataPtr + 1
-				end
-				dataPtr = dataPtr + 1 -- Skip over null char
 
-				table.insert(args, substring)
-			end
+		for i = 1, net.ReadUInt(16) do
+			args[i] = net.ReadBool() and net.ReadColor(false)
+				or net.ReadString()
 		end
 
 		-- Display the parsed message
