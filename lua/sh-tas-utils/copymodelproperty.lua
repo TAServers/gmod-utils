20855dcf4bcf5ee68f881251ef9def39a1d65dfe
Name <adamnejm@gmail.com>
2023-12-15 20:23:52 +0100

    Simplify filter expression + linter stfu

--- lua/sh-tas-utils/copymodelproperty.lua
+++ lua/sh-tas-utils/copymodelproperty.lua
@@ -1,17 +1,15 @@
-
-AddCSLuaFile()
-
 properties.Add("copy_model", {
 	MenuLabel = "Copy Model",
 	Order = 2137,
 	MenuIcon = "icon16/page_copy.png",
 	Filter = function(self, ent, ply)
-		if not ent or not ent:IsValid() or ent:IsPlayer() then return end
-		return true
+		return ent and ent:IsValid() and not ent:IsPlayer()
 	end,
 	Action = function(self, ent)
 		local mdl = ent:GetModel()
-		if not mdl then return end
+		if not mdl then
+			return
+		end
 		SetClipboardText(mdl)
 		surface.PlaySound("garrysmod/content_downloaded.wav")
 	end,
