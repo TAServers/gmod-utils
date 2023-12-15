b892349dace44c07c78743f23c7d4b1b684f5372
Name <adamnejm@gmail.com>
2023-12-15 21:08:23 +0100

    Linter stfu

--- lua/weapons/hands.lua
+++ lua/weapons/hands.lua
@@ -1,28 +1,34 @@
-SWEP.PrintName      = "Hands"
-SWEP.Author     	= "TAS Team"
-SWEP.Contact      	= "https://taservers.com/"
-SWEP.Instructions   = "Right-click while walking and crouching to toggle the crosshair"
-SWEP.IconOverride   = "entities/weapon_fists.png"
-SWEP.Spawnable    	= true
+SWEP.PrintName = "Hands"
+SWEP.Author = "TAS Team"
+SWEP.Contact = "https://taservers.com/"
+SWEP.Instructions = "Right-click while walking and crouching to toggle the crosshair"
+SWEP.IconOverride = "entities/weapon_fists.png"
+SWEP.Spawnable = true
 SWEP.AdminSpawnable	= false
 
-SWEP.Slot         	= 1
-SWEP.SlotPos      	= 1
-SWEP.HoldType       = "normal"
-SWEP.ViewModel      = "models/weapons/c_arms.mdl"
-SWEP.Primary.Ammo   = "none"
+SWEP.Slot = 1
+SWEP.SlotPos = 1
+SWEP.HoldType = "normal"
+SWEP.ViewModel = "models/weapons/c_arms.mdl"
+SWEP.Primary.Ammo = "none"
 SWEP.Secondary.Ammo = "none"
 
 if CLIENT then
-	SWEP.DrawAmmo         = false
-	SWEP.DrawCrosshair	  = true
-	SWEP.WepSelectIcon    = surface.GetTextureID("gui/faceposer_indicator")
+	SWEP.DrawAmmo = false
+	SWEP.DrawCrosshair = true
+	SWEP.WepSelectIcon = surface.GetTextureID("gui/faceposer_indicator")
 	SWEP.BounceWeaponIcon = false
 	
-	function SWEP:DrawWorldModel() end
-	function SWEP:DrawWorldModelTranslucent() end
+	function SWEP:DrawWorldModel()
+	end
+	
+	function SWEP:DrawWorldModelTranslucent()
+	end
 else
-	function SWEP:ShouldDropOnDie() return false end
+	function SWEP:ShouldDropOnDie()
+		return false
+	end
+	
 	function SWEP:OnDrop()
 		if SERVER then
 			self:Remove()
@@ -36,10 +42,13 @@ function SWEP:Initialize()
 	self:SetSequence("ragdoll") -- Along with the viewmodel, makes the hands invisible
 end
 
-function SWEP:PrimaryAttack() end
+function SWEP:PrimaryAttack()
+end
+
 function SWEP:SecondaryAttack()
-	if not IsFirstTimePredicted() then return end
-	if not self.Owner:IsWalking() or not self.Owner:Crouching() then return end
+	if not IsFirstTimePredicted() or not self.Owner:IsWalking() or not self.Owner:Crouching() then
+		return
+	end
 	self.DrawCrosshair = not self.DrawCrosshair
 	self:SetNextSecondaryFire(CurTime() + 0.3)
 end
