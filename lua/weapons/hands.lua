411c47f3cf09e4df89848ab2cdc3193507803b31
Name <adamnejm@gmail.com>
2023-12-15 21:04:54 +0100

    Get rid of bunch of unnecessary crap

--- lua/weapons/hands.lua
+++ lua/weapons/hands.lua
@@ -1,68 +1,45 @@
-SWEP.Author     	       = "PAC3 Team & TAS Team"
-SWEP.Contact      	       = "https://taservers.com/"
-SWEP.Purpose      	       = ""
-SWEP.Instructions          = "Right-click while walking and crouching to toggle the crosshair"
-SWEP.PrintName             = "Hands"
-SWEP.IconOverride          = "entities/weapon_fists.png"
-SWEP.Spawnable    	       = true
-SWEP.AdminSpawnable	       = false
+SWEP.PrintName      = "Hands"
+SWEP.Author     	= "TAS Team"
+SWEP.Contact      	= "https://taservers.com/"
+SWEP.Instructions   = "Right-click while walking and crouching to toggle the crosshair"
+SWEP.IconOverride   = "entities/weapon_fists.png"
+SWEP.Spawnable    	= true
+SWEP.AdminSpawnable	= false
 
-SWEP.AutoSwitchTo	       = true
-SWEP.AutoSwitchFrom	       = true
-SWEP.Weight 		       = 1
-SWEP.DrawAmmo              = false
-SWEP.DrawCrosshair	       = true
-SWEP.DrawWeaponInfoBox     = true
-SWEP.SlotPos      	       = 1
-SWEP.Slot         	       = 1
-SWEP.HoldType              = "normal"
-SWEP.ViewModel             = "models/weapons/c_arms.mdl"
+SWEP.Slot         	= 1
+SWEP.SlotPos      	= 1
+SWEP.HoldType       = "normal"
+SWEP.ViewModel      = "models/weapons/c_arms.mdl"
+SWEP.Primary.Ammo   = "none"
+SWEP.Secondary.Ammo = "none"
 
-SWEP.Primary.ClipSize      = -1
-SWEP.Primary.DefaultClip   = -1
-SWEP.Primary.Automatic     = false
-SWEP.Primary.Ammo          = "none"
-
-SWEP.Secondary.ClipSize    = -1
-SWEP.Secondary.DefaultClip = -1
-SWEP.Secondary.Automatic   = false
-SWEP.Secondary.Ammo        = "none"
-
-function SWEP:DrawHUD() end
-function SWEP:DrawWorldModel() end
-function SWEP:DrawWorldModelTranslucent() end
-function SWEP:CanPrimaryAttack() return false end
-function SWEP:CanSecondaryAttack() return false end
-function SWEP:Reload() return false end
-function SWEP:Holster() return true  end
-function SWEP:ShouldDropOnDie() return false end
-
-SWEP.WepSelectIcon = surface.GetTextureID("gui/faceposer_indicator")
-SWEP.BounceWeaponIcon = false
+if CLIENT then
+	SWEP.DrawAmmo         = false
+	SWEP.DrawCrosshair	  = true
+	SWEP.WepSelectIcon    = surface.GetTextureID("gui/faceposer_indicator")
+	SWEP.BounceWeaponIcon = false
+	
+	function SWEP:DrawWorldModel() end
+	function SWEP:DrawWorldModelTranslucent() end
+else
+	function SWEP:ShouldDropOnDie() return false end
+	function SWEP:OnDrop()
+		if SERVER then
+			self:Remove()
+		end
+	end
+end
 
 function SWEP:Initialize()
-	if self.SetHoldType then
-		self:SetHoldType("normal")
-	else
-		self:SetWeaponHoldType("normal")
-	end
-
-	-- Paired with SWEP.ViewModel = "models/weapons/c_arms.mdl" to make the arms invisible
-	self:SetSequence("ragdoll")
 	self:DrawShadow(false)
+	self:SetHoldType("normal")
+	self:SetSequence("ragdoll") -- Along with the viewmodel, makes the hands invisible
 end
 
-function SWEP:OnDrop()
-	if SERVER then
-		self:Remove()
-	end
-end
-
+function SWEP:PrimaryAttack() end
 function SWEP:SecondaryAttack()
 	if not IsFirstTimePredicted() then return end
 	if not self.Owner:IsWalking() or not self.Owner:Crouching() then return end
 	self.DrawCrosshair = not self.DrawCrosshair
 	self:SetNextSecondaryFire(CurTime() + 0.3)
 end
-
-weapons.Register(SWEP, "hands", true)
