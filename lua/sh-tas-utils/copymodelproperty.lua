4cb33bcfd24dccb993537d74978ffa573d42d9c3
Name <adamnejm@gmail.com>
2023-12-15 20:28:04 +0100

    Get rid of unused values

--- lua/sh-tas-utils/copymodelproperty.lua
+++ lua/sh-tas-utils/copymodelproperty.lua
@@ -2,10 +2,10 @@ properties.Add("copy_model", {
 	MenuLabel = "Copy Model",
 	Order = 2137,
 	MenuIcon = "icon16/page_copy.png",
-	Filter = function(self, ent, ply)
+	Filter = function(_, ent)
 		return ent and ent:IsValid() and not ent:IsPlayer()
 	end,
-	Action = function(self, ent)
+	Action = function(_, ent)
 		local mdl = ent:GetModel()
 		if not mdl then
 			return
