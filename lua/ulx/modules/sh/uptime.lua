c4a0d3cc887cabeab22745db485cfa408f22afa0
Name <adamnejm@gmail.com>
2023-12-16 01:24:18 +0100

    Enable linter

--- lua/ulx/modules/sh/uptime.lua
+++ lua/ulx/modules/sh/uptime.lua
@@ -3,23 +3,34 @@ local function plural(value, noun)
 end
 
 local startTime = SysTime()
-local uptimeCommand = ulx.command(
-	"Chat",
-	"ulx uptime",
-	function()
-		local time_data = string.FormattedTime(SysTime() - startTime)
-		local h, m, s = time_data.h, time_data.m, time_data.s
-		
-		if h < 1 and m < 1 then
-			ulx.fancyLog("Server has been running for #i #s", s, plural(s, "second"))
-		elseif h < 1 then
-			ulx.fancyLog("Server has been running for #i #s and #i #s", m, plural(m, "minute"), s, plural(s, "second"))
-		else
-			ulx.fancyLog("Server has been running for #i #s and #i #s", h, plural(h, "hour"), m, plural(m, "minute"))
-		end
-	end,
-	"!uptime"
-)
+local uptimeCommand = ulx.command("Chat", "ulx uptime", function()
+	local time_data = string.FormattedTime(SysTime() - startTime)
+	local h, m, s = time_data.h, time_data.m, time_data.s
+
+	if h < 1 and m < 1 then
+		ulx.fancyLog(
+			"Server has been running for #i #s",
+			s,
+			plural(s, "second")
+		)
+	elseif h < 1 then
+		ulx.fancyLog(
+			"Server has been running for #i #s and #i #s",
+			m,
+			plural(m, "minute"),
+			s,
+			plural(s, "second")
+		)
+	else
+		ulx.fancyLog(
+			"Server has been running for #i #s and #i #s",
+			h,
+			plural(h, "hour"),
+			m,
+			plural(m, "minute")
+		)
+	end
+end, "!uptime")
 
 uptimeCommand:defaultAccess(ULib.ACCESS_ALL)
 uptimeCommand:help("Prints the server uptime")
