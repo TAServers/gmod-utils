fabf8380e1bb588f8fd468d1fe5f2dc8e8fc5b81
Name <adamnejm@gmail.com>
2023-12-28 01:02:16 +0100

    Fix removed ropes count

--- lua/ulx/modules/sh/cleanropes.lua
+++ lua/ulx/modules/sh/cleanropes.lua
@@ -2,14 +2,16 @@ local cleanRopes = ulx.command(
 	TASUtils.Category,
 	"ulx cleanropes",
 	function(calling_ply)
+		local count = 0
 		local ropes = ents.FindByClass("keyframe_rope")
 		for _, v in ipairs(ropes) do
 			if v.Ent1 and v.Ent1:IsWorld() and v.Ent2 and v.Ent2:IsWorld() then
 				v:Remove()
+				count = count + 1
 			end
 		end
 
-		ulx.fancyLogAdmin(calling_ply, "#A cleaned up #i world ropes", #ropes)
+		ulx.fancyLogAdmin(calling_ply, "#A cleaned up #i world ropes", count)
 	end,
 	"!cleanropes"
 )
