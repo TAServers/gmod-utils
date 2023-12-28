e999290da43a8443f89e59c11cf56c12928ea504
Name <adamnejm@gmail.com>
2023-12-28 02:01:18 +0100

    Use the %H:%M:%S format

--- lua/ulx/modules/sh/uptime.lua
+++ lua/ulx/modules/sh/uptime.lua
@@ -1,33 +1,13 @@
-local function plural(value, noun)
-	return value ~= 1 and noun .. "s" or noun
-end
-
 local startTime = CurTime()
 local uptimeCommand = ulx.command("Chat", "ulx uptime", function()
-	local uptime = string.FormattedTime(CurTime() - startTime)
-	if uptime.h < 1 and uptime.m < 1 then
-		ulx.fancyLog(
-			"Server has been running for #i #s",
-			uptime.s,
-			plural(uptime.s, "second")
-		)
-	elseif uptime.h < 1 then
-		ulx.fancyLog(
-			"Server has been running for #i #s and #i #s",
-			uptime.m,
-			plural(uptime.m, "minute"),
-			uptime.s,
-			plural(uptime.s, "second")
-		)
-	else
-		ulx.fancyLog(
-			"Server has been running for #i #s and #i #s",
-			uptime.h,
-			plural(uptime.h, "hour"),
-			uptime.m,
-			plural(uptime.m, "minute")
-		)
-	end
+	local uptimeSeconds = CurTime() - startTime
+	local uptimeFormatted = string.format(
+		"%02i:%02i:%02i",
+		math.floor(uptimeSeconds / 3600),
+		math.floor(uptimeSeconds % 3600 / 60),
+		math.floor(uptimeSeconds % 60)
+	)
+	ulx.fancyLog("Server has been running for #s", uptimeFormatted)
 end, "!uptime")
 
 uptimeCommand:defaultAccess(ULib.ACCESS_ALL)
